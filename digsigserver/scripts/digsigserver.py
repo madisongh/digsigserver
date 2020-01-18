78642b77001e4f3b19177e3682931201e87ffcfe
Matt Madison <matt@madison.systems>
2020-01-18 10:44:09 -0800

    scripts: add script to run server from command line
    and move the test script to the scripts directory.

---
+++ digsigserver/scripts/digsigserver.py
@@ -0,0 +1,24 @@
+import argparse
+import os
+from digsigserver.server import app
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a', '--address', help='address to host on', default='0.0.0.0')
+    parser.add_argument('-p', '--port', help='port to host on', type=int, default=9999)
+    parser.add_argument('-d', '--debug', help='enable debug logging', action='store_true')
+    args = parser.parse_args()
+    if not os.getenv('DIGSIGSERVER_KEYFILE_URI'):
+        raise RuntimeError('Environment variable DIGSIGSERVER_KEYFILE_URI not set')
+    app.run(host=args.address, port=args.port, debug=args.debug)
+
+if __name__ == '__main__':
+    try:
+        main()
+        sys.exit(0)
+    except SystemExit:
+        pass
+    except Exception:
+        import traceback
+        traceback.print_exc(5)
+        sys.exit(1)
