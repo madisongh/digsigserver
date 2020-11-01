e57005e941390ab207e047e5f2e59743d165839f
Matt Madison <matt@madison.systems>
2020-11-01 10:21:24 -0800

    Fix cleanup of temporary response files
    We were using the automatic deletion of NamedTemporaryFile()s
    to ensure that tarballs created with tegra and module signing
    results were getting cleaned up.  This worked OK with Sanic
    versions 19.12 and earlier; with later versions, the files
    get closed in the sign_handler functions before the
    responses are sent, leading to FileNotFound exceptions.
    
    Fix this by disabling auto-deletion of the temp files and
    stashing the name in the request ctx field, then deleting
    it after the request/response has been completed by installing
    a wrapper function for the normal Sanic request handler.
    
    Signed-off-by: Matt Madison <matt@madison.systems>

--- digsigserver/server.py
+++ digsigserver/server.py
@@ -2,6 +2,7 @@ import asyncio
 import tempfile
 import re
 import os
+from functools import partial, update_wrapper
 
 from sanic import Sanic, request
 from sanic.log import logger
@@ -22,10 +23,61 @@ class CodesignSanicDefaults:
     LOG_LEVEL = 'INFO'
 
 
+"""
+If a temporary file is being streamed back in response,
+we need a way to remove that file once the response is
+complete.  Sanic provides no built-in way to do this,
+so borrow the following wrapping technique from the
+Sanic Plugins Framework project to wrap the normal
+request handler function.
+"""
+
+
+async def my_handle_request(real_handler, req, write_cb, stream_cb):
+    """
+    Wraps the normal handle_request function so we can check
+    if a filename has been set at req.ctx; if so, that's a
+    temporary file that needs to be deleted when we're done.
+    :param real_handler: normal handle_request function
+    :param req: request object
+    :param write_cb: write callback
+    :param stream_cb: stream callback
+    :return:
+    """
+    cancelled = False
+    try:
+        _ = await real_handler(req, write_cb, stream_cb)
+    except asyncio.CancelledError as ce:
+        cancelled = ce
+    except BaseException as be:
+        raise
+    finally:
+        if req.ctx:
+            os.unlink(req.ctx)
+            logger.info("Removed {}".format(req.ctx))
+        if cancelled:
+            raise cancelled
+
+
+def wrap_handle_request(app_):
+    """
+    Uses functools to wrap the handle_request method
+    in the app object.
+    :param app_: Sanic app
+    :return: callable
+    """
+    orig_handle_request = app_.handle_request
+    return update_wrapper(partial(my_handle_request, orig_handle_request), my_handle_request)
+
+
+"""
+Actual initialization happens here
+"""
 app = Sanic(name='digsigserver', load_env=False)
 app.config.from_object(CodesignSanicDefaults)
 app.config.load_environment_vars(prefix='DIGSIGSERVER_')
 logger.setLevel(app.config.get("LOG_LEVEL"))
+app.handle_request = wrap_handle_request(app)
 
 
 def config_get(item: str, default_value=None) -> str:
@@ -72,12 +124,19 @@ async def sign_handler_tegra(req: request):
             else:
                 result = await asyncio.get_running_loop().run_in_executor(None, s.sign, envvars, workdir)
             if result:
-                with tempfile.NamedTemporaryFile() as outfile:
-                    if await asyncio.get_running_loop().run_in_executor(None, utils.repack_files,
-                                                                        workdir, outfile.name):
-                        return await file_stream(outfile.name,
-                                                 mime_type="application/octet-stream",
-                                                 filename="signed-artifact.tar.gz")
+                # Since file streaming happens asynchronously, the temp file we create here
+                # could (will) get deleted when closed in this function unless we use delete=False.
+                # We want the file to get removed after the response has been sent, so set req.ctx
+                # to the temp file's path name so our request handler wrapper deletes it after
+                # processing the response.
+                outfile = tempfile.NamedTemporaryFile(delete=False)
+                req.ctx = outfile.name
+                outfile.close()
+                if await asyncio.get_running_loop().run_in_executor(None, utils.repack_files,
+                                                                    workdir, outfile.name):
+                    return await file_stream(outfile.name,
+                                             mime_type="application/octet-stream",
+                                             filename="signed-artifact.tar.gz")
     return text("Signing error", status=500)
 
 
@@ -95,12 +154,16 @@ async def sign_handler_modules(req: request):
         if await asyncio.get_running_loop().run_in_executor(None, utils.extract_files, workdir, f):
             result = await asyncio.get_running_loop().run_in_executor(None, s.sign, workdir)
             if result:
-                with tempfile.NamedTemporaryFile() as outfile:
-                    if await asyncio.get_running_loop().run_in_executor(None, utils.repack_files,
-                                                                        workdir, outfile.name):
-                        return await file_stream(outfile.name,
-                                                 mime_type="application/octet-stream",
-                                                 filename="signed-artifact.tar.gz")
+                # See the sign_handler_tegra function for an explanation of what's going on
+                # with temp file handling here.
+                outfile = tempfile.NamedTemporaryFile(delete=False)
+                req.ctx = outfile.name
+                outfile.close()
+                if await asyncio.get_running_loop().run_in_executor(None, utils.repack_files,
+                                                                    workdir, outfile.name):
+                    return await file_stream(outfile.name,
+                                             mime_type="application/octet-stream",
+                                             filename="signed-artifact.tar.gz")
     return text("Signing error", status=500)
 
 
