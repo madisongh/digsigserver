e6b31b4206b968f969b66f29ae2555ef5401965a
Matt Madison <matt@madison.systems>
2021-02-04 06:25:22 -0800

    Bump version to 0.3.6.
    Signed-off-by: Matt Madison <matt@madison.systems>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='digsigserver',
-    version='0.3.5',
+    version='0.3.6',
     packages=find_packages(),
     license='MIT',
     author='Matt Madison',
