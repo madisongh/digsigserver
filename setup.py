dbd865c2bafe69510f4da41e473e4a3adb037f3e
Matt Madison <matt@madison.systems>
2022-01-24 04:50:51 -0800

    Switch to using setup.cfg and pyproject.toml
    Signed-off-by: Matt Madison <matt@madison.systems>

--- setup.py
+++ setup.py
@@ -1,16 +1,2 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='digsigserver',
-    version='0.7.2',
-    packages=find_packages(),
-    license='MIT',
-    author='Matt Madison',
-    author_email='matt@madison.systems',
-    entry_points={
-        'console_scripts': [
-            'digsigserver = digsigserver.scripts.digsigserver:main',
-        ]
-    },
-    install_requires=['sanic>=21.12']
-)
+from setuptools import setup
+setup()
