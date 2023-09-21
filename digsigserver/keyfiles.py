dc3c0a1729603996a83523ca9e6d754c641419f4
Chad McQuillen <chad.mcquillen@lexmark.com>
2023-09-21 09:16:16 -0400

    keyfiles: add 'uefi' key file type
    Signed-off-by: Chad McQuillen <chad.mcquillen@lexmark.com>

--- digsigserver/keyfiles.py
+++ digsigserver/keyfiles.py
@@ -6,7 +6,7 @@ from . import utils
 
 
 class KeyFiles:
-    signing_types = ['tegrasign', 'imxsign', 'kmodsign', 'mender', 'swupdate', 'opteesign', 'rksign']
+    signing_types = ['tegrasign', 'imxsign', 'kmodsign', 'mender', 'swupdate', 'opteesign', 'rksign', 'uefisign']
 
     def __init__(self, app: Sanic, signtype: str, machine_or_distro: str):
         if signtype not in self.signing_types:
