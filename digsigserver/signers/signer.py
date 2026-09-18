d3717ea847275996709e2cedd0ed4f341b1babec
Matt Madison <matt@madison.systems>
2026-09-18 12:58:44 -0700

    signer: update for keyfiles change, extend run_command
    * Pass the current working directory to the KeyFiles constructor,
      which now wants a parent directory for the keyfiles temp dir.
    
    * Extend the run_command method so a caller can pass in a bytestring
      as input and can capture output to a file, possibly base64-encoded,
      for use with a new signing method.
    
    Signed-off-by: Matt Madison <matt@madison.systems>

--- digsigserver/signers/signer.py
+++ digsigserver/signers/signer.py
@@ -1,3 +1,4 @@
+import base64
 import os
 import subprocess
 from typing import Optional
@@ -10,7 +11,7 @@ class Signer:
 
     keytag = 'Unknown'
 
-    def __init__(self, app: Sanic, workdir: str, key_selector: Optional[str] = None,
+    def __init__(self, app: Sanic, workdir: str, key_selector: str,
                  backend: Optional[str] = None, load_keys: bool = True):
         self.app = app
         self.workdir = workdir
@@ -18,32 +19,46 @@ class Signer:
         self.backend = backend or 'ssl'
         self.keys = None
         if load_keys:
-            self.keys = KeyFiles(app, self.keytag, key_selector)
+            self.keys = KeyFiles(app, self.keytag, key_selector, parent_dir=workdir)
 
     def ensure_keys_loaded(self) -> KeyFiles:
         if not self.keys:
-            self.keys = KeyFiles(self.app, self.keytag, self.key_selector)
+            self.keys = KeyFiles(self.app, self.keytag, self.key_selector, parent_dir=self.workdir)
         return self.keys
 
     def sign(self, *args, **kwargs) -> bool:
         raise RuntimeError("unimplemented sign method")
 
-    def run_command(self, cmd: list, cleanup: bool = True, env: Optional[dict] = None) -> bool:
+    def run_command(self, cmd: list, cleanup: bool = True,
+                    env: Optional[dict] = None, input_bytes: Optional[bytes] = None,
+                    output_file: Optional[str] = None, b64encode_output: bool = False) -> bool:
         if not env:
             env = os.environ
         try:
             logger.info("PATH={}".format(env.get('PATH')))
             logger.info("Running: {}".format(cmd))
-            proc = subprocess.run(cmd, stdin=subprocess.DEVNULL, cwd=self.workdir,
-                                  env=env, check=True, capture_output=True,
-                                  encoding='utf-8')
-            logger.debug("stdout: {}".format(proc.stdout))
-            logger.debug("stderr: {}".format(proc.stderr))
+            if input_bytes is not None:
+                proc = subprocess.run(cmd, input=input_bytes, cwd=self.workdir,
+                                      env=env, check=True, capture_output=True)
+            else:
+                proc = subprocess.run(cmd, stdin=subprocess.DEVNULL, cwd=self.workdir,
+                                      env=env, check=True, capture_output=True)
+            if output_file is not None:
+                if b64encode_output:
+                    with open(output_file, 'w+b') as f:
+                        f.write(base64.b64encode(proc.stdout))
+                else:
+                    with open(output_file, 'w+') as f:
+                        f.write(proc.stdout.decode('utf-8'))
+                logger.debug("stdout sent to {}{}".format(output_file, ' (base64 encoded)' if b64encode_output else ''))
+            else:
+                logger.debug("stdout: {}".format(proc.stdout.decode('utf-8')))
+            logger.debug("stderr: {}".format(proc.stderr.decode('utf-8')))
         except subprocess.CalledProcessError as e:
             if cleanup and self.keys:
                 self.keys.cleanup()
-            logger.warning("signing error: {}".format(e.stderr))
-            logger.warning("stdout: {}".format(e.stdout))
+            logger.warning("signing error: {}".format(e.stderr.decode('utf-8')))
+            logger.warning("stdout: {}".format(e.stdout.decode('utf-8')))
             logger.warning("return code: {}".format(e.returncode))
             return False
         if cleanup and self.keys:
